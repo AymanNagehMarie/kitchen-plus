99b15e7d7b6ac7ff72612d5d22d93a96606769f6
ayman <ayman.nageh.dev@gmail.com>
2021-03-09 11:10:51 +0400

    add drawer

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { Button } from "react-native";
 import { createAppContainer, createSwitchNavigator } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
+import { createDrawerNavigator } from "react-navigation-drawer";
 import { createBottomTabNavigator } from "react-navigation-tabs";
-import { FontAwesome } from "@expo/vector-icons";
+import { FontAwesome, MaterialIcons } from "@expo/vector-icons";
 
 import CartItemScreen from "./src/screens/CartItemScreen";
 import AddClientScreen from "./src/screens/AddClientScreen";
@@ -18,8 +20,9 @@ import SalesOrderHistoryScreen from "./src/screens/SalesOrderHistoryScreen";
 import SubscribeScreen from "./src/screens/SubscribeScreen";
 import HomeScreen from "./src/screens/HomeScreen";
 import Header from "./src/components/Header";
+import { Switch } from "react-native-gesture-handler";
 
-const clientFlow = createBottomTabNavigator({
+const tabClientNav = createBottomTabNavigator({
   Products: createStackNavigator({ ProductScreen }),
   productFlow: createStackNavigator({
     CartItems: CartItemScreen,
@@ -31,36 +34,61 @@ const clientFlow = createBottomTabNavigator({
   Profile: createStackNavigator({ ProfileScreen }),
 });
 
-clientFlow.navigationOptions = {
-  headerTitle: () => <Header title="client test" />,
-  // title: "Tracks",
-};
-
-const switchNavigator = createSwitchNavigator({
-  Home: HomeScreen,
-  guestFlow: createBottomTabNavigator({
-    Products: createStackNavigator({ Products: ProductScreen }),
-    login: createStackNavigator({
-      Login: LoginScreen,
-      Subscribe: SubscribeScreen,
-    }),
+const tabDeliveryNav = createBottomTabNavigator({
+  orders: createStackNavigator({
+    deliveryList: DeliveryListScreen,
+    deliveryDetail: DeliveryOrderDetailScreen,
   }),
-
-  salesFlow: createBottomTabNavigator({
-    orders: createStackNavigator({
-      salesList: SalesOrderHistoryScreen,
-    }),
-    add: createStackNavigator({ AddClientScreen }),
-    Profile: createStackNavigator({ ProfileScreen }),
-  }),
-  deliveryFlow: createBottomTabNavigator({
-    orders: createStackNavigator({
-      deliveryList: DeliveryListScreen,
-      deliveryDetail: DeliveryOrderDetailScreen,
-    }),
-    Profile: createStackNavigator({ ProfileScreen }),
-  }),
-
-  clientFlow,
+  Profile: createStackNavigator({ ProfileScreen }),
 });
-export default createAppContainer(switchNavigator);
+
+const tabSalesNav = createBottomTabNavigator({
+  orders: createStackNavigator({
+    salesList: SalesOrderHistoryScreen,
+  }),
+  add: createStackNavigator({ AddClientScreen }),
+  Profile: createStackNavigator({ ProfileScreen }),
+});
+const loginStack = createStackNavigator({
+  Login: LoginScreen,
+  Subscribe: SubscribeScreen,
+});
+const tabGuestNav = createBottomTabNavigator({
+  Products: createStackNavigator({ ProductScreen }),
+  login: loginStack,
+});
+
+const CurrentNavigator = (role) => {
+  switch (role) {
+    case "client":
+      return createDrawerNavigator({
+        client: tabClientNav,
+        CartItem: CartItemScreen,
+        Profile: ProfileScreen,
+      });
+    case "delivery":
+      return createDrawerNavigator({
+        orders: tabDeliveryNav,
+        Profile: createStackNavigator({ ProfileScreen }),
+      });
+    case "sales":
+      return createDrawerNavigator({
+        sales: tabSalesNav,
+        add: AddClientScreen,
+        Profile: ProfileScreen,
+      });
+    default:
+      return createDrawerNavigator({
+        products: createStackNavigator({ ProductScreen }),
+        Login: createStackNavigator({ Login: LoginScreen }),
+      });
+  }
+};
+export default createAppContainer(CurrentNavigator(""));
+// const switchNavigator = createDrawerNavigator({
+//   Home: HomeScreen,
+//   products: guestFlow,
+//   sales: salesFlow,
+//   orders: deliveryFlow,
+//   client: clientFlow,
+// });
