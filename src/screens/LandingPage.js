918399dc78f57f96fb71ea4b5bde726ec240d7b3
eng-MahmoudGhonim <eng.mahmoudghonim@gmail.com>
2021-03-16 15:20:51 +0200

    loader

--- src/screens/LandingPage.js
+++ src/screens/LandingPage.js
@@ -1,19 +1,47 @@
 // Dynamically Set Drawer/Sidebar Options in React Navigation Drawer
 // https://aboutreact.com/dynamically-change-sidebar-options/
 
-import * as React from "react";
-import { Button, View, Text, SafeAreaView } from "react-native";
+import React,{useState,useEffect} from "react";
+import { Button, View, Text, SafeAreaView,StyleSheet } from "react-native";
+import Spinner from 'react-native-loading-spinner-overlay';
 
 const LandingPage = ({ navigation }) => {
-  setTimeout(
-    function() {
-      navigation.navigate("drawerStack", { userType: "guest" });
-    }
-    .bind(this),
-    1
-  )
+  const [loading, setLoading] = useState(true);
+  useEffect(()=>{
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+            navigation.navigate("drawerStack", { userType: "guest" });
+
+    }, 500);  },[])
+
+
+//  setTimeout(
+//     function() {
+//      {startLoading}
+//       navigation.navigate("drawerStack", { userType: "guest" });
+//     }
+//     .bind(this),
+//     1
+//   )
+  // setTimeout(
+  //   function() {
+  //     navigation.navigate("drawerStack", { userType: "guest" });
+  //   }
+  //   .bind(this),
+  //   1
+  // )
   return (
     <SafeAreaView style={{ flex: 1 }}>
+
+<Spinner
+          //visibility of Overlay Loading Spinner
+          visible={loading}
+          //Text with the Spinner
+          textContent={'Loading...'}
+          //Text style of the Spinner Text
+          textStyle={styles.spinnerTextStyle}
+        />
       {/* <View style={{ flex: 1, padding: 16 }}>
         <View
           style={{
@@ -86,5 +114,10 @@ const LandingPage = ({ navigation }) => {
     </SafeAreaView>
   );
 };
+const styles = StyleSheet.create({
 
+  spinnerTextStyle: {
+    color: '#FFF',
+  },
+});
 export default LandingPage;
